2fae6b73a33d2e9ec8f97a18ac1074821ceb18e9
kpaska <kelvin.paska@gmail.com>
2016-08-10 13:14:28 +0200

    testing jenkins file

--- .jenkins/run_tests.sh
+++ .jenkins/run_tests.sh
@@ -8,8 +8,3 @@ echo "Starting test"
 # is the service up and running?
 nc -z objectstore 80
 echo "HTTP server up and running"
-
-# do the tile locations work as expected?
-curl -s https://ffb7a5a57dd34cc49436abc510cad162.objectstore.eu/tiles/lufo2011_rd_cache_EPSG28992/fake-tile.txt | grep 'I AM A FAKE objectstore'
-echo "HTTP server serving objectstore"
-
